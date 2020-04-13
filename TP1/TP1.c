83b922a6821158b1291f3316f9cca8f520a9602c
caroblanco <62450177+caroblanco@users.noreply.github.com>
2020-04-13 16:22:17 -0300

    Co-authored-by: Ezequiel-H <horowitzeze@gmail.com> Co-authored-by: Camila Guillan <camiguillan@gmail.com> Co-authored-by: EstebanDer <estebanderghazarian@gmail.com>

--- TP1/TP1.c
+++ TP1/TP1.c
@@ -6,116 +6,91 @@
 
 int aQueColumnaVoy(char cCaracter, FILE *salida){
     
-    if (cCaracter = '0') {
+    int columna;
+
+    if (cCaracter == '0') {
         columna = 0;
-        fputc(cCaracter, salida);
         }
     else 
         if (cCaracter >= '1' &&  cCaracter <= '7') {
             columna = 1;
-            fputc(cCaracter, salida);
         }
     else 
-        if (caracter <=57 && caracter>=56) {
+        if (cCaracter == '8' || cCaracter == '9') {
             columna = 2;
-            fputc(cCaracter, salida);
         }
     else 
-        if (caracter == 88 || caracter == 120){
+        if (cCaracter == 'x' || cCaracter == 'X') {
             columna = 3;
-            fputc(cCaracter, salida);
         }
     else 
-        if (caracter>= 65 && caracter<=70 || caracter>= 102 && caracterASCII<=97){
+        if ((cCaracter >= 'a' && cCaracter <= 'f')|| (cCaracter >= 'A' && cCaracter <= 'F')) {
             columna = 4;
-            fputc(cCaracter, salida);
         }
     else{
             columna = 5;
-            fputc(cCaracter, salida);
         }
     return columna;
+}
 
-    
 int main(){
-    
+
     int TT [7][6] =
-    { 
-    //   0  1-7 8-9 x-X lets error
-        {q2, q1, q1, q6, q6, q6},   //q0
-        {q1, q1, q1, q6, q6, q6},   //q1
-        {q3, q5, q6, q3, q6, q6},   //q2
-        {q4, q4, q4, q6, q4, q6},   //q3
-        {q4, q4, q4, q6, q4, q6},   //q4
-        {q5, q5, q6, q6, q6, q6},   //q5
-        {q6, q6, q6, q6, q6, q6},   //q6
+   {
+    //   0   1-7  8-9  x-X  lets error
+        {2,   1,   1,   6,   6,   6},   //q0
+        {1,   1,   1,   6,   6,   6},   //q1
+        {3,   5,   6,   3,   6,   6},   //q2
+        {4,   4,   4,   6,   4,   6},   //q3
+        {4,   4,   4,   6,   4,   6},   //q4
+        {5,   5,   6,   6,   6,   6},   //q5
+        {6,   6,   6,   6,   6,   6},   //q6
     };
     
-    char caracter, muestra ;
-    int caracterASCII, noReconocido = 0, estado = 0;
+    char caracter, muestra, tipo;
+    int caracterASCII, noReconocido = 0, estado = 0, columna;
     FILE *archivo;
     FILE *salida;
-    aQueColumnaVoy(caracter ; salida)
-    while(fread(&caracter, si0eof(caracter), 1, archivo)){
-        
-        caracterASCII = caracter;
-            for(int c=0; c<6; ++c){
-                if(TT[0][c]==0){
-                    
-                    
-                 }
-            }
-
-    }
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     archivo = fopen ("numeros.txt", "r+");
     salida = fopen ("num_salida.txt", "wb+");
-
+    
     if(!archivo){
-        printf ("no se pudo leer el archivo");
+        printf ("No se pudo leer el archivo");
     }
     else{
-
         while(fread(&caracter, sizeof(caracter), 1, archivo)){
-    
+            if (caracter !=  ','){
+                 fputc(caracter, salida);
+                 aQueColumnaVoy(caracter, salida);
+                 estado = TT[estado][aQueColumnaVoy(caracter,salida)];
+            }
+            else {
+                switch (estado)
+                {
+                case 1:
+                    fputs("--> Es Decimal \n", salida);
+                    break;
+                case 4:
+                    fputs("--> Es Hexadecimal \n", salida);
+                    break;
+                case 2:
+                    fputs("--> Es 0 \n", salida);
+                    break;
+                case 5:
+                    fputs("--> Es Octal \n", salida);
+                    break;
+                default:
+                    fputs("--> No Fue Reconocido \n", salida);
+                    break;
+                }
+
+                estado = 0;
+            }
         }
     }
 
-    
+    fclose(archivo);
+    fclose(salida);
     return 0;
 }
