618c1c6e6acf37c4877d2803ee05cb958bd9587c
caroblanco <62450177+caroblanco@users.noreply.github.com>
2020-04-13 13:09:31 -0300

    Co-authored-by: Ezequiel-H <horowitzeze@gmail.com> Co-authored-by: Camila Guillan <camiguillan@gmail.com> Co-authored-by: EstebanDer <estebanderghazarian@gmail.com>

---
+++ TP1/TP1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    int TT [7][6] =
+    { 
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+    };
+ 
+los escucho con voz de marciano
+jajaja
+    return 0;
+}
