34cdbf83e6ebb06735eecb625b8a0c122101b85b
Carolina Blanco <carolina.blancos@gmail.com>
2020-05-13 18:09:25 -0300

    Co-authored-by: Ezequiel-H <horowitzeze@gmail.com> Co-authored-by: EstebanDer <estebanderghazarian@gmail.com> Co-authored-by: Camila Guillan <camiguillan@gmail.com>

--- TP2/TP2.0.c
+++ TP2/TP2.0.c
@@ -3,8 +3,8 @@
 #include <string.h>
 
 typedef struct _nodo {
-   int valor;
-   struct _nodo *siguiente;
+   char valor;
+   struct _nodo * siguiente;
 } tipoNodo;
 
 typedef tipoNodo * pNodo;
@@ -39,39 +39,29 @@ char pop(Pila *pila) {
    return v;
 }
 
-int aQueColumnaVoy (char cCaracter, Pila *pila, char cimaPila){
+int aQueColumnaVoy (char cCaracter){
     
     int columna;
-
-    // Push(&pila, 10);
-    // printf("%d, ", Pop(&pila)); 
    
     if (cCaracter == '0') 
     {
         columna = 0;
-        push (&pila, cimaPila);
     }
     else if (cCaracter >= '1' &&  cCaracter <= '9') 
     {
         columna = 1;
-        push (&pila,cimaPila);
     }
     else if (cCaracter == '+' || cCaracter == '-' || cCaracter == '*' || cCaracter == '/')
     {
         columna = 2;
-        push (&pila,cimaPila);
-        
     }
     else if (cCaracter == '(') 
     {
         columna = 3;
-        push (&pila,cimaPila);
-        push (&pila,cimaPila);
     }
     else if (cCaracter == ')')
     {
         columna = 4;
-        push (&pila,cimaPila);
     }
     else
     {
@@ -80,21 +70,50 @@ int aQueColumnaVoy (char cCaracter, Pila *pila, char cimaPila){
     return columna; 
 }
 
+void pilasuper (char cCaracter, Pila **pila, char cimaPila){
+   
+    if (cCaracter == '0') 
+    {
+        push (*pila, cimaPila);
+    }
+    else if (cCaracter >= '1' &&  cCaracter <= '9') 
+    {
+        push (*pila,cimaPila);
+    }
+    else if (cCaracter == '+' || cCaracter == '-' || cCaracter == '*' || cCaracter == '/')
+    {
+        push (*pila,cimaPila);
+        
+    }
+    else if (cCaracter == '(') 
+    {
+        push (*pila,cimaPila);
+        push (*pila,cimaPila);
+    }
+    else if (cCaracter == ')')
+    {
+        push (*pila,cimaPila);
+    }
+    else
+    {
+    }
+     
+}
 int main(){
 int TT [2][4][6];
 // CI = 0 CIMA DE LA PILA = $       (ﾉ^_^)ﾉ    
 //        CI  E  C                 C: 0 => 0    1 => 1-9  2=>{+,-,/,*} 3=>(  4=>)  5=>ERROR
         
         TT[0][0][0] =3;        // $, q0 , 0
-        TT[0][0][1] =1;       // $, q0 , 1
+        TT[0][0][1] =1;        // $, q0 , 1
         TT[0][0][2] =3;        // $, q0 , 2
         TT[0][0][3] =0;        // $, q0 , 3
         TT[0][0][4] =3;        // $, q0 , 4
         TT[0][0][5] =3;        // $, q0 , 5
         TT[0][1][0] =1;        // $, q1 , 0
-        TT[0][1][1] =1;        // $, q1 , 1                       
+        TT[0][1][1] =1;        // $, q1 , 1               
         TT[0][1][2] =0;        // $, q1 , 2                      
-        TT[0][1][3] =3;        // $, q1 , 3                       
+        TT[0][1][3] =3;        // $, q1 , 3                      
         TT[0][1][4] =3;        // $, q1 , 4
         TT[0][1][5] =3;        // $, q1 , 5
         TT[0][2][0] =3;        // $, q2 , 0
@@ -104,7 +123,7 @@ int TT [2][4][6];
         TT[0][2][4] =3;        // $, q2 , 4
         TT[0][2][5] =3;        // $, q2 , 5
         TT[0][3][0] =3;        // $, q3 , 0 
-        TT[0][3][1] =3;        // $, q3 , 1
+        TT[0][3][1] =3;        // $, q3 , 1 
         TT[0][3][2] =3;        // $, q3 , 2
         TT[0][3][3] =3;        // $, q3 , 3
         TT[0][3][4] =3;        // $, q3 , 4
@@ -119,8 +138,8 @@ int TT [2][4][6];
         TT[1][0][5] =3;        // R, q0 , 5
         TT[1][1][0] =1;        // R, q1 , 0
         TT[1][1][1] =1;        // R, q1 , 1
-        TT[1][1][2] =0;        // R, q1 , 2
-        TT[1][1][3] =3;        // R, q1 , 3
+        TT[1][1][2] =0;        // R, q1 , 2                             
+        TT[1][1][3] =3;        // R, q1 , 3                                
         TT[1][1][4] =2;        // R, q1 , 4
         TT[1][1][5] =3;        // R, q1 , 5
         TT[1][2][0] =3;        // R, q2 , 0
@@ -137,30 +156,37 @@ int TT [2][4][6];
         TT[1][3][5] =3;        // R, q3 , 5 
     
 
-char expresion[50], caracter, cimaPila;
-int estado = 0, columna = 0, ci = 100;
+char expresion[3], caracter, cimaPila;
+int estado = 0, columna = 0, ci = 0, error=0;
 Pila *pila = NULL;
 
 printf ("Porfis ingrese una expresion \n");
 scanf ("%c",expresion);
 
-for (int i=0; i<50 ; i++) //recorremos la expresion
+push (*pila, '$'); 
+
+
+for (int i=0; i<3 ; i++) //recorremos la expresion 
 {
     
     caracter = expresion[i];
-    columna = aQueColumnaVoy(caracter, &pila, cimaPila);
+    columna = aQueColumnaVoy(caracter);
 
-    if (estado != 3 && columna != 5 )  //osea, no hay error   
+    if (estado != 3 && columna != 5 )  //osea, no hay error   :3  
     {
-        cimaPila = pop (&pila);
+        cimaPila = pop (*pila);
         if (cimaPila == 'R'){
             ci = 1;
         } else {
             ci = 0;
         }
-            
+        pilasuper (caracter, *pila, cimaPila);
         estado = TT[ci][estado][columna];
         printf ("%c", caracter);
+    } 
+    else if (error != 1){
+        printf ("%c  --> ERROR", caracter);
+        error = 1;
     }
 }
 }
